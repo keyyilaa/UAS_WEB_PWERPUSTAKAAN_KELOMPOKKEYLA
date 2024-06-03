8221fbae6d3c8c759760d3a6fe94154796102705
keyyilaa <keylaimanda07@gmail.com>
2024-06-04 06:17:29 +0700

    final commit

---
+++ resources/views/partials/register/footer.blade.php
@@ -0,0 +1,26 @@
+<footer class="footer py-5">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-8 mx-auto text-center mb-4 mt-2">
+                <a href="" target="_blank" class="text-secondary me-xl-4 me-4">
+                    <span class="text-lg fab fa-youtube"></span>
+                </a>
+                <a href="" target="_blank" class="text-secondary me-xl-4 me-4">
+                    <span class="text-lg fab fa-instagram"></span>
+                </a>
+                <a href="" target="_blank" class="text-secondary me-xl-4 me-4">
+                    <span class="text-lg fab fa-facebook"></span>
+                </a>
+                <a href="" target="_blank" class="text-secondary me-xl-4 me-4">
+                    <span class="text-lg fab fa-github"></span>
+                </a>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-8 mx-auto text-center mt-1">
+                <p class="mb-0 text-secondary"> Social Media Account~
+                </p>
+            </div>
+        </div>
+    </div>
+</footer>
